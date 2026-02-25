f16dd6a0da3d792eed0cbb531039690735b8acc1
Sean W. Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2026-02-25 16:13:29 +0000

    Edit comments and footer

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,20 +1,16 @@
 import React, { useEffect, useState } from "react";
 import Counter from "./Counter.jsx";
 
-
-// ---------> REMINDER: ANNOTATE HOW CODE WORKS FOR FUTURE SELF !!!!!!!!!!!!!!!!! <---------
-
 // "Time" IN MY COMMENTS refers to the number of seconds since the page loaded. Not to be confused with the variable called "time", which in theory is the same thing. The "time" variable is passed in from Counter.jsx in the Components folder.
 
 //create your first component
 const Home = ({ time }) => {
-
   // New code
   const [counter, setCounter] = useState(0);
-  
+
   // New code
   useEffect(() => {
-    console.log("Hello from the useEffect")
+    console.log("Hello from the useEffect");
   }, []);
 
   const stringTime = time.toString(); // time as a string WITHOUT leading zeros
@@ -23,22 +19,39 @@ const Home = ({ time }) => {
   // Keeps adding leading zeros until stringTimeWithZeros has 6 digits
   for (let i = 1; i < leadingZeros; i++) {
     stringTimeWithZeros = "0" + stringTimeWithZeros;
-  } 
+  }
 
   return (
     <div className="container text-center">
-      {/* Each digit of time displayed in a span, from left to right, starting with leading zeros, such that the entire number always has 6 digits */}
+      {/* Clock symbol: */}
       <span className="digit1 border bg-secondary p-3 m-2">&#128344;</span>
-      <span className="digit1 border bg-secondary p-3 m-2">{stringTimeWithZeros[0]}</span>
-      <span className="digit2 border bg-secondary p-3 m-2">{stringTimeWithZeros[1]}</span>
-      <span className="digit2 border bg-secondary p-3 m-2">{stringTimeWithZeros[2]}</span>
-      <span className="digit2 border bg-secondary p-3 m-2">{stringTimeWithZeros[3]}</span>
-      <span className="digit2 border bg-secondary p-3 m-2">{stringTimeWithZeros[4]}</span>
-      <span className="digit2 border bg-secondary p-3 m-2">{stringTimeWithZeros[5]}</span>
-      <div className="text-white fixed-bottom">Made by Sean Hammond, student at 4Geeks Academy</div>
+      <span className="digit1 border bg-secondary p-3 m-2">
+        {stringTimeWithZeros[0]}
+      </span>
+      <span className="digit2 border bg-secondary p-3 m-2">
+        {stringTimeWithZeros[1]}
+      </span>
+      <span className="digit2 border bg-secondary p-3 m-2">
+        {stringTimeWithZeros[2]}
+      </span>
+      <span className="digit2 border bg-secondary p-3 m-2">
+        {stringTimeWithZeros[3]}
+      </span>
+      <span className="digit2 border bg-secondary p-3 m-2">
+        {stringTimeWithZeros[4]}
+      </span>
+      <span className="digit2 border bg-secondary p-3 m-2">
+        {stringTimeWithZeros[5]}
+      </span>
+      <footer className="text-white fixed-bottom">
+        <p>© 2025-2026 Hammond Studios.</p>
+        <p>
+          Contributors and mentors: Alejandro Sanchez, Ryan Castanier, Alexander
+          Ayala-Palacin, Thomas Brito-Bronfield
+        </p>
+      </footer>
     </div>
   );
 };
 
 export default Home;
-// Project made by Sean Hammond, student at 4Geeks Academy
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,28 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+import "bootstrap";
 
 // index.css'
-import '../styles/index.css'
+import "../styles/index.css";
 
 // components
-import Home from './components/Home';
+import Home from "./components/Home";
 let seconds = 0;
 
 setInterval(() => {
-  seconds = seconds + 1
-setInterval(()=> setSeconds(seconds + 1), 1000)
+  seconds = seconds + 1;
+  setInterval(() => setSeconds(seconds + 1), 1000);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home time = {seconds}/>
-  </React.StrictMode>,
-)
-}
-  ,1000)
-
-
-
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <Home time={seconds} />
+    </React.StrictMode>,
+  );
+}, 1000);
